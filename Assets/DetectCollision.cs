b3e44fdd4227aae170de2add793fc562cd87118e
SGokul2005 <poogokul9@gmail.com>
2025-11-12 10:39:43 +0530

    success

--- Assets/DetectCollision.cs
+++ Assets/DetectCollision.cs
@@ -4,16 +4,20 @@ using UnityEngine;
 
 public class DetectCollision : MonoBehaviour
 {
+    
     void Start()
     {
 
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
 
     }
-    
-
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(gameObject);
+        Destroy(other.gameObject);
+    }
 }
